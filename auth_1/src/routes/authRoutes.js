f1d79fa8bd45360b195fcb121ced2864511cc506
Chaitanya Malik <chaitanya.malik@sabpaisa.in>
2024-09-16 11:54:38 +0530

    auth full

--- auth_1/src/routes/authRoutes.js
+++ auth_1/src/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import Router from 'express'
-import { loginUser, registerUser } from '../controller/authController.js'
+import { getProfile, loginUser, registerUser } from '../controller/authController.js'
+import { authMiddleware } from '../middelware/authMiddleware.js'
 
 const router = Router()
 
 
 router.post('/register',registerUser)
 router.post('/login',loginUser)
+router.get('/profile',[authMiddleware],getProfile)
 
 
 export default router
